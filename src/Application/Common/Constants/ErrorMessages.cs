5ada43e0685b1e76773018dc7bffffd358f35d8c
coderrayhan <rakibrayhan8@gmail.com>
2024-07-27 16:44:22 +0600

    Identity Role Service

---
+++ src/Application/Common/Constants/ErrorMessages.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Common.Constants;
+public static class ErrorMessages
+{
+    #region Auth & Admin
+
+    public const string WRONG_USERNAME_PASSWORD = "Username or password wrong.";
+    public const string USER_NOT_FOUND = "User not found";
+    public const string UNABLE_DELETE_USER = "Unable to delete the user";
+    public const string UNABLE_CREATE_USER = "Unable to create the user";
+    public const string UNABLE_UPDATE_USER = "Unable to update the user";
+    public const string ROLE_NOT_FOUND = "Role not found";
+    public const string UNABLE_DELETE_ROLE = "Unable to delete the role";
+    public const string UNABLE_CREATE_ROLE = "Unable to create the role";
+    public const string UNABLE_UPDATE_ROLE = "Unable to update the role";
+    public const string UNABLE_UPDATE_PERMISSION = "Unable to update the permission";
+
+    public const string TOKEN_DID_NOT_MATCH = "Token did not match any users.";
+    public const string TOKEN_NOT_ACTIVE = "Token Not Active.";
+    public const string INVALID_TOKEN = "Invalid Token";
+
+
+    #endregion
+
+
+    public const string NotFound = "Not Found";
+    public const string EntityNotFound = "Entity Not Found";
+}
---
+++ src/Infrastructure/Identity/Services/IdentityRoleService.cs
@@ -0,0 +1,110 @@
+﻿using Application.Common.Abstractions.Identity;
+using Application.Common.Constants;
+using Domain.Shared;
+using Infrastructure.Identity.Permissions;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Identity.Services;
+internal class IdentityRoleService(
+    RoleManager<IdentityRole> roleManager,
+    IdentityContext context,
+    ILogger<IdentityRoleService> logger) 
+    : IIdentityRoleService
+{
+    public async Task<Result> AddorRemoveClaimsToRoleAsync(string id, List<string> permissions, CancellationToken cancellationToken)
+    {
+        var role = await roleManager.FindByIdAsync(id);
+
+        if (role is null) return Result.Failure(Error.Failure("Role.Update", ErrorMessages.ROLE_NOT_FOUND));
+
+        var existedPermissions = await context.RoleClaims.Where(e => e.RoleId == role.Id).ToListAsync(cancellationToken);
+
+        context.RoleClaims.RemoveRange(existedPermissions);
+
+        var newPermissions = permissions.Select(x => new IdentityRoleClaim<string>
+        {
+            RoleId = role.Id,
+            ClaimType = CustomClaimTypes.Permission,
+            ClaimValue = x
+        });
+
+        context.RoleClaims.AddRange(newPermissions);
+
+        await context.SaveChangesAsync(cancellationToken);
+
+        return Result.Success();
+    }
+
+    public async Task<Result<string>> CreateRoleAsync(string name, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var role = new IdentityRole
+            {
+                Name = name,
+                NormalizedName = name.ToUpper()
+            };
+
+            var result = await roleManager.CreateAsync(role);
+
+            return result.Succeeded ? Result.Success(role.Id)
+                : Result.Failure<string>(Error.Failure("Role.Create", MapErrors(result.Errors)));
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Error occured to save role");
+            return Result.Failure<string>(Error.Failure("Role.Create", "Error occured to save role"));
+        }
+    }
+
+    public async Task<Result> DeleteRoleAsync(string id, CancellationToken cancellationToken)
+    {
+        var role = await roleManager.FindByIdAsync(id);
+
+        if (role is null) return Result.Failure(Error.Failure("Role.Delete", ErrorMessages.ROLE_NOT_FOUND));
+        
+        var result = await roleManager.DeleteAsync(role);
+
+        return result.Succeeded ?
+            Result.Success()
+            : Result.Failure(Error.Failure("Role.Delete", ErrorMessages.UNABLE_DELETE_ROLE));
+    }
+
+    public async Task<Result<string>> GetRoleAsync(string id, CancellationToken cancellationToken)
+    {
+        var role = await roleManager.FindByIdAsync(id);
+
+        if (role is null) return Result.Failure<string>(Error.Failure("Role.Delete", ErrorMessages.ROLE_NOT_FOUND));
+
+        return Result.Success(await roleManager.GetRoleNameAsync(role));
+    }
+
+    public async Task<Result> UpdateRoleAsync(string id, string name, CancellationToken cancellationToken)
+    {
+        var role = await roleManager.FindByIdAsync(id);
+
+        if (role is null) return Result.Failure(Error.Failure("Role.Update", ErrorMessages.ROLE_NOT_FOUND));
+
+        role.Name = name;
+        role.NormalizedName = name.ToUpper();
+
+        var result = await roleManager.UpdateAsync(role);
+
+        return result.Succeeded ?
+            Result.Success() :
+            Result.Failure(Error.Failure("Role.Update", MapErrors(result.Errors)));
+    }
+
+    private string MapErrors(IEnumerable<IdentityError> errors)
+    {
+        var errorList = errors.Select(e => $"{e.Code} : {e.Description}");
+        return string.Join("; ", errorList);
+    }
+}
