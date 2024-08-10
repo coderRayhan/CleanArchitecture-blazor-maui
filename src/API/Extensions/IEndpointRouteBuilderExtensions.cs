ff97a7bd560e6b3f15a21fcf53ee9a81ccabd794
coderrayhan <rakibrayhan8@gmail.com>
2024-08-11 00:16:14 +0600

    Extensions

---
+++ src/API/Extensions/IEndpointRouteBuilderExtensions.cs
@@ -0,0 +1,47 @@
+﻿using Ardalis.GuardClauses;
+
+namespace API.Extensions;
+
+public static class IEndpointRouteBuilderExtensions
+{
+    public static IEndpointRouteBuilder MapGet(this IEndpointRouteBuilder builder, Delegate handler, string pattern = "", string? withName = null)
+    {
+        Guard.Against.AnonymousMethod(handler);
+
+        builder.MapGet(pattern, handler)
+            .WithName(withName ?? handler.Method.Name);
+
+        return builder;
+    }
+
+    public static IEndpointRouteBuilder MapPost(this IEndpointRouteBuilder builder, Delegate handler, string pattern = "", string? withName = null)
+    {
+        Guard.Against.AnonymousMethod(handler);
+
+        builder.MapPost(pattern, handler)
+            .WithName (withName ?? handler.Method.Name);
+
+        return builder;
+    }
+
+    public static IEndpointRouteBuilder MapPut(this IEndpointRouteBuilder builder, Delegate handler, string pattern = "", string? withName = null)
+    {
+        Guard.Against.AnonymousMethod(handler);
+
+        builder.MapPut(pattern, handler)
+            .WithName(withName ?? handler.Method.Name);
+
+        return builder;
+    }
+
+    public static IEndpointRouteBuilder MapDelete(this IEndpointRouteBuilder builder, Delegate handler, string pattern, string? withName = null)
+    {
+        Guard.Against.AnonymousMethod(handler);
+
+        builder.MapDelete(pattern, handler)
+            .WithName(withName ?? handler.Method.Name);
+
+        return builder;
+
+    }
+}
---
+++ src/API/Extensions/ResultExtensions.cs
@@ -0,0 +1,54 @@
+﻿using Domain.Shared;
+
+namespace API.Extensions;
+
+public static class ResultExtensions
+{
+    public static IResult ToProblemDetails(this Result result)
+    {
+        if (result.IsSuccess) throw new InvalidOperationException();
+
+        return Results.Problem(
+            statusCode: GetStatusCode(result.Error.ErrorType),
+            title: GetTitle(result.Error.ErrorType),
+            type: GetType(result.Error.ErrorType),
+            detail: result.Error.Message,
+            extensions: new Dictionary<string, object?>
+            {
+                {"errors", result.Error }
+            });
+    }
+
+    private static int GetStatusCode(ErrorType errorType) =>
+        errorType switch
+        {
+            ErrorType.Validation => StatusCodes.Status400BadRequest,
+            ErrorType.Unauthorized => StatusCodes.Status401Unauthorized,
+            ErrorType.Forbidden => StatusCodes.Status403Forbidden,
+            ErrorType.NotFound => StatusCodes.Status404NotFound,
+            ErrorType.Conflict => StatusCodes.Status409Conflict,
+            _ => StatusCodes.Status500InternalServerError
+        };
+
+    private static string GetTitle(ErrorType errorType) =>
+        errorType switch
+        {
+            ErrorType.Validation => "Bad Request",
+            ErrorType.Unauthorized => "Unauthorized",
+            ErrorType.Forbidden => "Forbidden",
+            ErrorType.NotFound => "Not Found",
+            ErrorType.Conflict => "Conflict",
+            _ => "Internal Server Error"
+        };
+
+    private static string GetType(ErrorType errorType) =>
+        errorType switch
+        {
+            ErrorType.Validation => "https://tools.ietf.org/html/rfc7231#section-6.5.1",
+            ErrorType.Unauthorized => "https://tools.ietf.org/html/rfc7235#section-3.1",
+            ErrorType.Forbidden => "https://tools.ietf.org/html/rfc7231#section-6.5.3",
+            ErrorType.NotFound => "https://tools.ietf.org/html/rfc7231#section-6.5.4",
+            ErrorType.Conflict => "https://tools.ietf.org/html/rfc7231#section-6.5.8",
+            _ => "https://tools.ietf.org/html/rfc7231#section-6.6.1"
+        };
+}
