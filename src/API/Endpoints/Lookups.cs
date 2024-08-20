5e5163ec24cc2ca7a4aaea26db9e7208a7c849ef
coderrayhan <rakibrayhan8@gmail.com>
2024-08-20 07:36:14 +0600

    Get all lookups endpoints

---
+++ src/API/Endpoints/Lookups.cs
@@ -0,0 +1,28 @@
+﻿using API.Infrastructure;
+using Application.Common.Models;
+using Application.Features.Lookups.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using API.Extensions;
+
+namespace API.Endpoints;
+
+public sealed class Lookups : EndpointGroupBase
+{
+    public override void Map(WebApplication app)
+    {
+        var group = app.MapGroup(this);
+
+        group.MapPost("GetAll", GetAll)
+            .WithName("GetLookups")
+            .Produces<PaginatedList<LookupResponse>>(StatusCodes.Status200OK);
+    }
+
+    private async Task<IResult> GetAll(
+        ISender sender,
+        [FromBody] GetLookupListQuery query)
+    {
+        var result = await sender.Send(query);
+        return TypedResults.Ok(result.Value);
+    }
+}
---
+++ src/Application/Features/Lookups/Validators/CreateLookupCommandValidator.cs
@@ -0,0 +1,26 @@
+﻿using Application.Features.Lookups.Commands;
+
+namespace Application.Features.Lookups.Validators;
+internal class CreateLookupCommandValidator
+    : AbstractValidator<CreateLookupCommand>
+{
+    public CreateLookupCommandValidator()
+    {
+        RuleFor(e => e.Name)
+            .NotEmpty()
+            .MaximumLength(200);
+
+        RuleFor(e => e.NameBN)
+            .NotEmpty()
+            .MaximumLength(200);
+
+        RuleFor(e => e.Code)
+            .NotEmpty()
+            .MaximumLength(10);
+    }
+
+    //private async Task<bool> BeUniqueName(string name, CancellationToken cancellationToken)
+    //{
+
+    //}
+}
