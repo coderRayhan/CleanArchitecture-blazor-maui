b0592c6d8272ad8ddc4691e65b1e6f2fd1a1dd9d
coderrayhan <rakibrayhan8@gmail.com>
2024-08-22 07:24:21 +0600

    nswag configuration

--- src/API/DependencyInjection.cs
+++ src/API/DependencyInjection.cs
@@ -2,7 +2,10 @@
 using API.Services;
 using Application.Common.Abstractions.Identity;
 using Microsoft.AspNetCore.Mvc;
+using NSwag;
+using NSwag.Generation.Processors.Security;
 using System.Text.Json;
+using ZymLabs.NSwag.FluentValidation;
 
 namespace Microsoft.Extensions.DependencyInjection;
 
@@ -15,7 +18,10 @@ public static class DependencyInjection
         AddScopedServices(services);
         AddExceptionHandlers(services);
         AddHttpContextAccessor(services);
+        AddFluentValidationSchemaProcessor(services);
         ConfigureApiBehaviour(services);
+        AddOpenApiDocument(services);
+
         return services;
     }
 
@@ -50,6 +56,18 @@ public static class DependencyInjection
         services.AddExceptionHandler<GlobalExceptionHandler>();
     }
 
+    private static void AddFluentValidationSchemaProcessor(IServiceCollection services)
+    {
+        services.AddScoped(provider =>
+        {
+            var validationRules = provider.GetService<IEnumerable<FluentValidationRule>>();
+
+            var loggerFactory = provider.GetService<ILoggerFactory>();
+
+            return new FluentValidationSchemaProcessor(provider, validationRules, loggerFactory);
+        });
+    }
+
     private static void ConfigureApiBehaviour(IServiceCollection services)
     {
         services.Configure<ApiBehaviorOptions>(options =>
@@ -62,7 +80,20 @@ public static class DependencyInjection
         {
             settings.Title = "API";
 
+            var fluentValidationSchemaProcessor = sp.CreateScope()
+            .ServiceProvider.GetRequiredService<FluentValidationSchemaProcessor>();
 
+            settings.SchemaSettings.SchemaProcessors.Add(fluentValidationSchemaProcessor);
+
+            settings.AddSecurity("JWT", Enumerable.Empty<string>(), new OpenApiSecurityScheme
+            {
+                Type = OpenApiSecuritySchemeType.ApiKey,
+                Name = "Authorization",
+                In = OpenApiSecurityApiKeyLocation.Header,
+                Description = "Type into the textbox: Bearer {your JWT token}."
+            });
+
+            settings.OperationProcessors.Add(new AspNetCoreOperationSecurityScopeProcessor("JWT"));
         });
     }
 }
--- src/API/Program.cs
+++ src/API/Program.cs
@@ -7,10 +7,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
-});
 
 builder.Services.AddApplicationServices();
 builder.Services.AddInfrastructureServices(builder.Configuration);
@@ -21,7 +17,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    await app.IdentityInitialiseDatabaseAsync();
+    //await app.IdentityInitialiseDatabaseAsync();
 
     //app.UseSwagger();
     //app.UseSwaggerUI(op =>
@@ -34,6 +30,10 @@ if (app.Environment.IsDevelopment())
         options.Path = "/api";
         options.DocumentPath = "/api/specification.json";
     });
+}
+else 
+{
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
