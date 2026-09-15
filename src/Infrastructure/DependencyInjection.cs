f62394d45d64f63f43ad94b013e6062f2238a594
coderrayhan <rakibrayhan8@gmail.com>
2026-09-15 22:51:08 +0600

    dfs

--- src/Infrastructure/DependencyInjection.cs
+++ src/Infrastructure/DependencyInjection.cs
@@ -1,27 +1,9 @@
-﻿using Application.Common.Abstractions;
-using Application.Common.Abstractions.Caching;
-using Application.Common.Abstractions.Idempotency;
-using Application.Common.Abstractions.Identity;
-using Ardalis.GuardClauses;
-using Domain.Constants;
+﻿using Application.Common.Abstractions.Idempotency;
 using Infrastructure.Caching;
-using Infrastructure.Configurations;
 using Infrastructure.Identity;
-using Infrastructure.Identity.Model;
-using Infrastructure.Identity.OptionsSetup;
-using Infrastructure.Identity.Permissions;
-using Infrastructure.Identity.Services;
 using Infrastructure.Persistence;
 using Infrastructure.Persistence.Idempotency;
-using Infrastructure.Persistence.Interceptors;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Options;
-using StackExchange.Redis;
 
 namespace Microsoft.Extensions.DependencyInjection;
 public static class DependencyInjection
